dcb7bd8bd76e18d41d92201372c6b6c381415777
NaZ!ru <165497883+NaZiiru@users.noreply.github.com>
2026-02-23 09:51:59 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,5 +13,5 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 html_static_path = ['_static']
