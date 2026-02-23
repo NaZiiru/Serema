d10a1fa0451f9899345f6364176d4368544a78a0
NaZiiru <mouhamadou.diankha@cbao.sn>
2026-02-23 09:30:03 +0000

    config: readthedocs configuration

---
+++ docs/conf.py
@@ -0,0 +1,11 @@
+# -- Project information -----------------------------------------------------
+project = 'Serema'
+copyright = '2026, Serema'
+author = 'Serema'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
